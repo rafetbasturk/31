feb3cfaf55b78ff4170b7330ce78b2d257cebb82
Rafet <rafet@Rafet-MacBook-Air.local>
2021-03-07 21:28:54 +0300

    Changed gitignore.

--- script.js
+++ script.js
@@ -62,5 +62,5 @@ app.post("/", function (req, res) {
 })
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Server is running on port 3000.");
+    console.log("Server has started.");
 })
